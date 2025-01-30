258fb1c2e2fa275b0d288b60805a558f1368ca27
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:32:06 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,29 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        //byte
+        byte bytevalue=100;
+        System.out.println("byte value:"+bytevalue);
+        //int
+        int value=25;
+        System.out.println("int value:");
+        //float
+        float num=26.3f;
+        System.out.println("float num:"+num);
+        //long
+        long longvalue=100002348851l;
+        System.out.println("longvalue:"+longvalue);
+        //string
+        String mystring="helloworld";
+        System.out.println("string value:"+mystring);
+       //char
+       char mychar='s';
+       System.out.println("char value:"+mychar);
+       
+
+
+
+
+
+    }
+    
+}
